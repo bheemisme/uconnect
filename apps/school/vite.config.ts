6b135ba3d6f7c0e03acf49c95476658518abe53f
sudarshan <vemarapu.sudarshan@outlook.com>
2022-06-20 13:02:06 +0530

    adjusted vite config for production

--- apps/school/vite.config.ts
+++ apps/school/vite.config.ts
@@ -1,10 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "global": {},
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis", //<-- AWS SDK 
+      },
+    },
   },
-})
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill(),
+      ],
+      output: {
+        manualChunks: {
+          lodash: ['lodash']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1600
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser', // <-- Fix from above
+    },
+  }
+});
